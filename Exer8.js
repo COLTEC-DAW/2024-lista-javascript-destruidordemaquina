202f99cfe01fde6a28e02137214cc1cc8f0cd22c
Nicolle Durães <nicolleccoelhoduraes@gmail.com>
2024-07-03 10:46:40 -0300

    exer8

---
+++ Exer8.js
@@ -0,0 +1,22 @@
+function range(NUM1, NUM2)
+{
+    if (NUM1 >= NUM2) {
+        return [];
+    }
+    let resultado = [];
+
+    for (let num = NUM1; num <= NUM2; num++)
+    {
+        resultado.push(num);
+    }
+        return resultado;
+        
+}
+
+var NUM1 = parseInt(prompt("Digite o primeiro número:"));
+var NUM2 = parseInt(prompt("Digite o segundo número:"));
+
+resultado = range(NUM1, NUM2);
+
+console.log("Resultado", resultado);
+
